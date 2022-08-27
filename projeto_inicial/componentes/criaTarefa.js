2edc1485453d43ba9be48da237f5e427d4cc1b4b
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-26 21:09:00 -0300

    Criação do arquivo criar tarefas

--- projeto_inicial/componentes/criaTarefa.js
+++ projeto_inicial/componentes/criaTarefa.js
@@ -3,8 +3,7 @@ import BotaoDeleta from './deletaTarefa.js'
 
 export const handleNovoItem = (evento) => {
     evento.preventDefault()
-    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
-    const lista = document.querySelector('[data-list]')
+    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []    
     const input = document.querySelector('[data-form-input]')
     const valor = input.value
 
@@ -17,11 +16,7 @@ export const handleNovoItem = (evento) => {
         dataFormatada
     }
 
-    const tarefasAtualizadas = [...tarefas, dados]
-
-    const criaTarefa = Tarefa(dados)
-
-    lista.appendChild(criaTarefa)      
+    const tarefasAtualizadas = [...tarefas, dados]          
 
     localStorage.setItem("tarefas", JSON.stringify(tarefasAtualizadas))
 
@@ -29,7 +24,6 @@ export const handleNovoItem = (evento) => {
 }
     
 export const Tarefa = ({ valor, dataFormatada }) => {
-
     //evento.preventDefault()  
 
     const tarefa = document.createElement('li')

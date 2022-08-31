1b184bd5dcf2bbeffb87ef8193bd025fd5390471
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-31 19:50:47 -0300

    Atualização de arquivos

--- projeto_inicial/componentes/carregaTarefa.js
+++ projeto_inicial/componentes/carregaTarefa.js
@@ -1,17 +1,15 @@
+import { ordenaDatas, removeDatasRepetidas } from "../service/data.js"
 import { criaData } from "./criaData.js"
-
 export const carregaTarefa = () => { 
     const lista = document.querySelector('[data-list]')
 
     const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas'))||[]
 
-    lista.innerHTML = ""
-    tarefasCadastradas.forEach((tarefa) => {   
-        const dia = moment(tarefa.dataFormatada, 'DD/MM/YYYY') 
-        const diff = data.diff(dia) 
+    lista.innerHTML = " "
+    const dataUnicas = removeDatasRepetidas(tarefasCadastradas)
+    ordenaDatas(dataUnicas)
+    dataUnicas.forEach((dia) => {
         
-        if(diff == 0 ){ 
-            lista.appendChild(criaData(dia) 
-        
+        lista.appendChild(criaData(dia))
     })
 }

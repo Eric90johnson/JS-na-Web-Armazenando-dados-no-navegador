00cd9007a1372594a2cdb0486a37afa652142f10
Eric Johnson <95862960+Eric90johnson@users.noreply.github.com>
2022-08-30 21:39:27 -0300

    separando tarefas

--- projeto_inicial/componentes/carregaTarefa.js
+++ projeto_inicial/componentes/carregaTarefa.js
@@ -1,12 +1,17 @@
-import { Tarefa } from "./criaTarefa.js"
+import { criaData } from "./criaData.js"
 
 export const carregaTarefa = () => { 
     const lista = document.querySelector('[data-list]')
 
     const tarefasCadastradas = JSON.parse(localStorage.getItem('tarefas'))||[]
 
-    lista.innerHTML = " "
-    tarefasCadastradas.forEach((tarefa) => {
-        lista.appendChild(Tarefa(tarefa))
+    lista.innerHTML = ""
+    tarefasCadastradas.forEach((tarefa) => {   
+        const dia = moment(tarefa.dataFormatada, 'DD/MM/YYYY') 
+        const diff = data.diff(dia) 
+        
+        if(diff == 0 ){ 
+            lista.appendChild(criaData(dia) 
+        
     })
 }
--- projeto_inicial/componentes/criaData.js
+++ projeto_inicial/componentes/criaData.js
@@ -1,3 +1,21 @@
-export const criarData = () => {
+import {Tarefa} from './criaTarefa.js'
+
+export const criaData = (data) => {
+    const tarefas = JSON.parse(localStorage.getItem('tarefas')) || []
+    const dataTopo = document.createElement('li')
+    const conteudo = `<p class="content-data">${data.format('DD/MM/YYYY')} </p>` 
+
+    dataTopo.innerHTML = conteudo
+
+    tarefas.forEach((tarefa => {   
+        const dia = moment(tarefa.dataFormatada, 'DD/MM/YYYY') 
+        
+        const diff = data.diff(dia)
+        if(diff == 0 ) { 
+            lista.appendChild(criaData(dia)}
+
+                
+    })  
     
+    return dataTopo
 }
